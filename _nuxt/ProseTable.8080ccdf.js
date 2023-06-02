465f4cd6358d48b829051033df72ca6a7b1c9252
Yvan Sabay <sabayyvan2018@gmail.com>
2023-06-02 11:56:30 +0800

    added contact and project section but still not fully working or empty

---
+++ _nuxt/ProseTable.8080ccdf.js
@@ -0,0 +1,1 @@
+import{k as r,o,l as t,r as s}from"./entry.82e1017f.js";const a={};function l(e,n){return o(),t("table",null,[s(e.$slots,"default")])}const f=r(a,[["render",l]]);export{f as default};
--- nuxt/ProseHr.4e7ebfc9.js
+++
@@ -1,1 +0,0 @@
-import{k as e,o as r,l as c}from"./entry.26572601.js";const o={};function t(n,s){return r(),c("hr")}const _=e(o,[["render",t]]);export{_ as default};
